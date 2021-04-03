de3eab06fe00d2ddab168d52a2bb83278a4ff395
Alex Gogl <alex@gogl.io>
2021-04-03 21:30:53 +0200

    initial extension working

---
+++ background.js
@@ -0,0 +1,46 @@
+var toggle = false;
+var status = "off";
+var the_tab_id = "";
+
+function set_status() {
+  toggle = !toggle;
+  status = "off";
+  if (toggle) {
+    status = "on";
+  }
+}
+
+function toggle_extension(tab) {
+  // Set icon
+  chrome.browserAction.setIcon({
+    path: "icons/logo.png",
+    tabId: tab.id,
+  });
+
+  // Pass variable & execute script
+  chrome.tabs.executeScript({
+    code: 'var extension_status = "' + status + '"',
+  });
+  chrome.tabs.executeScript({ file: "inject.js" });
+  // Set the tab id
+  the_tab_id = tab.id;
+}
+
+function my_listener(tabId, changeInfo, tab) {
+  // If updated tab matches this one
+  if (
+    changeInfo.status == "complete" &&
+    tabId == the_tab_id &&
+    status == "on"
+  ) {
+    toggle_extension(tab);
+  }
+}
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.executeScript({ file: "extract.js" });
+  //   set_status();
+  //   toggle_extension(tab);
+});
+
+chrome.tabs.onUpdated.addListener(my_listener);
--- extract.js
+++ extract.js
@@ -1,53 +1,39 @@
-javascript: (function () {
-  // {
-  //     question: "",
-  //     answers: [
-  //       {
-  //         content: "blablabala",
-  //         isCorrect: false,
-  //       },
-  //     ],
-  //   }
+function extract() {
+  let questions = [];
 
-  function extract() {
-    let questions = [];
+  // 1. frage herauslesen
+  const frage = document.getElementsByClassName("panel-heading")[0].innerText;
 
-    // 1. frage herauslesen
-    const frage = document.getElementsByClassName("panel-heading")[0].innerText;
+  // 2. alle möglichen antworten herausfiltern
+  const possibleAnswers = document.getElementsByClassName("word-wrap");
 
-    // 2. alle möglichen antworten herausfiltern
-    const possibleAnswers = document.getElementsByClassName("word-wrap");
+  // 3. Text verschönern
+  const possibleAnswersTransformed = [];
 
-    // 3. Text verschönern
-    const possibleAnswersTransformed = [];
+  for (item of possibleAnswers) {
+    const color = window.getComputedStyle(item).color;
+    const isCorrect = color == "rgb(61, 148, 215)";
 
-    for (item of possibleAnswers) {
-      const color = window.getComputedStyle(item).color;
-      const isCorrect = color == "rgb(61, 148, 215)";
-
-      possibleAnswersTransformed.push({
-        content: item.innerText,
-        isCorrect: isCorrect,
-      });
-    }
-
-    questions.push({
-      question: frage,
-      answers: possibleAnswersTransformed,
+    possibleAnswersTransformed.push({
+      content: item.innerText,
+      isCorrect: isCorrect,
     });
-
-    return questions;
   }
 
-  const questions = extract();
+  questions.push({
+    question: frage,
+    answers: possibleAnswersTransformed,
+  });
 
-  // 4. auf antworten klicken
-  const answerButton = document.getElementById("sAnswer");
-  //   answerButton.click();
+  return questions;
+}
 
-  const nextQuestionButton = document.getElementById("sQuestionNext");
+const questions = extract();
 
-  console.log(questions);
+// 4. auf antworten klicken
+const answerButton = document.getElementById("sAnswer");
+//   answerButton.click();
 
-  //   alert(frage.innerHTML);
-})();
+const nextQuestionButton = document.getElementById("sQuestionNext");
+
+console.log(questions);
